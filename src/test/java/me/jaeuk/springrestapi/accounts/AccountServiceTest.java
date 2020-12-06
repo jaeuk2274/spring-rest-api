d708d25ce131fe5e47ca12d2ae82f43d498a1709
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-06 19:54:11 +0900

    Exception Test code

--- src/test/java/me/jaeuk/springrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/jaeuk/springrestapi/accounts/AccountServiceTest.java
@@ -1,19 +1,30 @@
 package me.jaeuk.springrestapi.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Set;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 
 @SpringBootTest
 @ActiveProfiles("text")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -22,6 +33,7 @@ public class AccountServiceTest {
     AccountRepository accountRepository;
 
     @Test
+    @DisplayName("스프링 시큐리티 학습테스트")
     public void findByUsername(){
         // Given
         String password ="jaeuk";
@@ -39,6 +51,30 @@ public class AccountServiceTest {
 
         // Then
         assertEquals(userDetails.getPassword(), password);
+    }
+
+    @Test
+    @DisplayName("방법1. 유저네임 없을 경우")
+    public void findByUsernameFail(){
+        String username = "notpeople@eamil.com";
+        try{
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        } catch (UsernameNotFoundException e){
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }
+
+    @Test
+    @DisplayName("방법2. 유저네임 없을 경우")
+    public void findByUsernameFail2(){
+        // Expected, 예상되는 예외를 미리 적어줘야 한다.
+        String username = "notpeople@eamil.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When , 해당 예외가 발생하지 않으면 에러
+        accountService.loadUserByUsername(username);
     }
 
 }
